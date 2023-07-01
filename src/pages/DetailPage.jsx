302ecc746a6b548c5cf0176aa52e923b97f06c34
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-06-30 23:02:10 -0300

    Add data by and time of comment

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -5,16 +5,21 @@ import Comment from "../components/Comment"
 function DetailPage({ params }) {
   const { data, isLoading, error } = useSWR(`story/${params.id}`, () => getItemInfo(params.id))
 
-  // console.log(data)
-
   if (error) return <div>failed to load</div>
 
   if (isLoading) return <div>loading...</div>
 
+  // console.log(data)
+
   return (
     <div>
       <h1>{data.title}</h1>
-      <h2>Comments:</h2>
+      <h2>By: {data.by}</h2>
+      <h3>
+        <a href={data.url}>{data.url}</a>
+      </h3>
+
+      <h3 style={{ borderBottom: "1px solid gray" }}>({data.descendants}) Comments:</h3>
       <ul>
         {data.kids.map((kid) => (
           <Comment key={kid} kidId={kid} />
